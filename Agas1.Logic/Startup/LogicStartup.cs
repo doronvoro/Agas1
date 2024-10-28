c0d85b06479f9402214bd0569bbee7c166f97654
doronvoro <doronvoro@gmail.com>
2024-10-28 22:40:35 +0200

    1

---
+++ Agas1.Logic/Startup/LogicStartup.cs
@@ -0,0 +1,18 @@
+﻿using Agas1.Logic.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Agas1.Logic
+{
+    public static class LogicStartup
+    {
+        public static void AddAgas1Logic(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<DistilleryContext>(options =>
+    options.UseSqlite("Data Source=distillery.db", b => b.MigrationsAssembly("Agas1.Logic")));
+
+            serviceCollection.AddScoped<DistilleryService>();
+            serviceCollection.AddScoped<LiquidTypeService>();
+        }
+    }
+}
